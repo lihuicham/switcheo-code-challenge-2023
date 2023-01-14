1b0a8a44a2c0e1c8e5c57e4dd69ac4eb6782fe59
Li Hui Cham <lihuicham@gmail.com>
2023-01-15 01:12:16 +0800

    bug/button ripple effect not working

---
+++ src/problem2/script.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('input[name="pay-button"]'); 
+
+button.addEventListener('click', function(e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+        ripples.remove()
+    }, 1000);
+})
+
