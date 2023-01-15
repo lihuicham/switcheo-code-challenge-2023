7645aef6e0fdaebf0162c3dc3162aaafab166ef0
Li Hui Cham <lihuicham@gmail.com>
2023-01-15 11:28:37 +0800

    feat/toast notification completed

--- src/problem2/script.js
+++ src/problem2/script.js
@@ -1,5 +1,9 @@
 const button = document.querySelector('input[name="pay-button"]'); 
+const toast = document.querySelector('.toast');
+const closeIcon = document.querySelector('.close');
+const progress = document.querySelector('.progress');
 
+// Button ripple effect code - but not working
 button.addEventListener('click', function(e) {
     let x = e.clientX - e.target.offsetLeft;
     let y = e.clientY - e.target.offsetTop;
@@ -12,5 +16,26 @@ button.addEventListener('click', function(e) {
     setTimeout(() => {
         ripples.remove()
     }, 1000);
-})
+});
 
+
+button.addEventListener('click', () => {
+    toast.classList.add('active');
+    progress.classList.add('active');
+
+    setTimeout(() => {
+        toast.classList.remove('active');
+    }, 4000) //1s = 1000 ms (animation time for progress is 4s)
+
+    setTimeout(() => {
+        progress.classList.remove('active');
+    }, 4300)
+});
+
+closeIcon.addEventListener('click', () => {
+    toast.classList.remove('active');
+
+    setTimeout(() => {
+        toast.classList.remove('active');
+    }, 300)
+})
