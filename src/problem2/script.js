e2db546c77fa1510dd1537a5a4bda4ad63d6e6ae
Li Hui Cham <lihuicham@gmail.com>
2023-01-15 16:06:47 +0800

    feat/form-validation completed

--- src/problem2/script.js
+++ src/problem2/script.js
@@ -7,43 +7,6 @@ const progress = document.querySelector('.progress');
 const popup = document.querySelector('.popup');
 const dismissButton = document.getElementById('dismiss-popup-button');
 
-// Button ripple effect code - but not working
-button.addEventListener('click', function(e) {
-    let x = e.clientX - e.target.offsetLeft;
-    let y = e.clientY - e.target.offsetTop;
-
-    let ripples = document.createElement('span');
-    ripples.style.left = x + 'px';
-    ripples.style.top = y + 'px';
-    this.appendChild(ripples);
-
-    setTimeout(() => {
-        ripples.remove()
-    }, 1000);
-});
-
-// ======== TOAST MESSAGE JS ========
-
-button.addEventListener('click', () => {
-    toast.classList.add('active');
-    progress.classList.add('active');
-    //popup.classList.add('active');
-
-    setTimeout(() => {
-        toast.classList.remove('active');
-    }, 4000) //1s = 1000 ms (animation time for progress is 4s)
-
-    setTimeout(() => {
-        progress.classList.remove('active');
-    }, 4300)
-
-    setTimeout(() => {
-        box.classList.remove('active');
-        form.classList.remove('active');
-        popup.classList.add('active');
-    }, 5000)
-});
-
 closeIcon.addEventListener('click', () => {
     toast.classList.remove('active');
 
@@ -69,37 +32,66 @@ const otp = document.getElementById('otp');
 let ethErrorMessage = document.querySelector('.eth-error-message');
 let amountErrorMessage = document.querySelector('.amount-error-message');
 let otpErrorMessage = document.querySelector('.otp-error-message');
-
-button.addEventListener('click', e => {
-    e.preventDefault();
-    validateETH();
-    validateAmount()
-    validateOTP();
-})
+let ethValue = eth.value.trim();
+let amountValue = amount.value.trim();
+let otpValue = otp.value.trim();
 
 const validateETH = () => {
-    const ethValue = eth.value.trim();
+    ethValue = eth.value.trim();
     if (ethValue === '') {
         ethErrorMessage.innerText = 'ETH address is required.';
+        return false;
     } else {
-        ethErrorMessage = '';
+        ethErrorMessage.innerText = '';
     }
 }
 
 const validateAmount = () => {
-    const amountValue = amount.value.trim();
+    amountValue = amount.value.trim();
     if (amountValue === '') {
         amountErrorMessage.innerText = 'Please enter the amount you wish to send.';
+        return false;
     } else {
-        amountErrorMessage = '';
+        amountErrorMessage.innerText = '';
     }
 }
 
 const validateOTP = () => {
-    const otpValue = otp.value.trim();
+    otpValue = otp.value.trim();
     if (otpValue === '') {
         otpErrorMessage.innerText = 'OTP value is required for authentication.';
+        return false;
     } else {
-        otpErrorMessage = '';
+        otpErrorMessage.innerText = '';
+    }
+}
+
+function validation() {
+    const ethBoolean = validateETH();
+    const amountBoolean = validateAmount();
+    const otpBoolean = validateOTP();
+
+    if (ethBoolean === false || amountBoolean === false || otpBoolean === false) {
+        return false;
+    } else {
+        form.submit();
+
+        // ======== TOAST MESSAGE JS ========
+        toast.classList.add('active');
+        progress.classList.add('active');
+
+        setTimeout(() => {
+            toast.classList.remove('active');
+        }, 4000) //1s = 1000 ms (animation time for progress is 4s)
+
+        setTimeout(() => {
+            progress.classList.remove('active');
+        }, 4300)
+
+        setTimeout(() => {
+            box.classList.remove('active');
+            form.classList.remove('active');
+            popup.classList.add('active');
+        }, 5000)
     }
 }
