c1fca9f41661435d6ba4ca99f0d9cc888c7416ab
Li Hui Cham <lihuicham@gmail.com>
2023-01-15 15:39:33 +0800

    Error messages added

--- src/problem2/script.js
+++ src/problem2/script.js
@@ -1,3 +1,5 @@
+const box = document.querySelector('.box');
+const form = document.querySelector('.form');
 const button = document.querySelector('input[name="pay-button"]'); 
 const toast = document.querySelector('.toast');
 const closeIcon = document.querySelector('.close');
@@ -25,7 +27,7 @@ button.addEventListener('click', function(e) {
 button.addEventListener('click', () => {
     toast.classList.add('active');
     progress.classList.add('active');
-    popup.classList.add('active');
+    //popup.classList.add('active');
 
     setTimeout(() => {
         toast.classList.remove('active');
@@ -34,6 +36,12 @@ button.addEventListener('click', () => {
     setTimeout(() => {
         progress.classList.remove('active');
     }, 4300)
+
+    setTimeout(() => {
+        box.classList.remove('active');
+        form.classList.remove('active');
+        popup.classList.add('active');
+    }, 5000)
 });
 
 closeIcon.addEventListener('click', () => {
@@ -47,4 +55,51 @@ closeIcon.addEventListener('click', () => {
 // ======== SUCCESS POPUP JS ========
 dismissButton.addEventListener('click', () => {
     popup.classList.remove('active');
+
+    setTimeout(() => {
+        location.reload();
+    }, 500)
 })
+
+// ======== FORM VALIDATION JS ========
+
+const eth = document.getElementById('eth');
+const amount = document.getElementById('amount');
+const otp = document.getElementById('otp');
+let ethErrorMessage = document.querySelector('.eth-error-message');
+let amountErrorMessage = document.querySelector('.amount-error-message');
+let otpErrorMessage = document.querySelector('.otp-error-message');
+
+button.addEventListener('click', e => {
+    e.preventDefault();
+    validateETH();
+    validateAmount()
+    validateOTP();
+})
+
+const validateETH = () => {
+    const ethValue = eth.value.trim();
+    if (ethValue === '') {
+        ethErrorMessage.innerText = 'ETH address is required.';
+    } else {
+        ethErrorMessage = '';
+    }
+}
+
+const validateAmount = () => {
+    const amountValue = amount.value.trim();
+    if (amountValue === '') {
+        amountErrorMessage.innerText = 'Please enter the amount you wish to send.';
+    } else {
+        amountErrorMessage = '';
+    }
+}
+
+const validateOTP = () => {
+    const otpValue = otp.value.trim();
+    if (otpValue === '') {
+        otpErrorMessage.innerText = 'OTP value is required for authentication.';
+    } else {
+        otpErrorMessage = '';
+    }
+}
