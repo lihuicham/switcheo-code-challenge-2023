9aedcfa8b608a7507d786d40990b37da2a6b97a0
Li Hui Cham <lihuicham@gmail.com>
2023-01-20 09:34:49 +0800

    Complete problem 3

--- src/problem3/datasource.js
+++ src/problem3/datasource.js
@@ -4,18 +4,19 @@
 // - price.quote() returns a substring of currency 
 
 class Datasource {
-    constructor() {
-
+    constructor(url) {
+      this.url = url
     }
 
     async getPrices() {
         try {
-          const response = await fetch('https://my-json-server.typicode.com/lihuicham/price-api/data');
+          const response = await fetch(this.url);
           if (!response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
           }
           const data = await response.json();
-          return data.prices;
+
+          return data.prices.map(price => new Price(price.buy, price.sell, price.id, price.pair, price.timestamp));
         }
         catch (error) {
           console.error(`Error message: ${error}`);
@@ -23,14 +24,26 @@ class Datasource {
     }
 }
 
+class Price {
+  constructor(buy, sell, id, pair, timestamp) {
+    this.buy = buy;
+    this.sell = sell;
+    this.id = id;
+    this.pair = pair; 
+    this.timestamp = timestamp;
+  }
 
-ds = new Datasource();
-ds.getPrices()
-    .then(prices => {
-        prices.forEach(price => {
-            console.log(`Mid price for ${ price.pair } is ${ price.mid() } ${ price.quote() }.`);
-        });
-    }).catch(error => {
-        console.error(error);
-    });
+  mid() {
+    const midprice =  (this.buy + this.sell) / 200;
+    return midprice;
+  }
+
+  quote() {
+    const substring = this.pair.substring(3, 6);
+    return substring;
+  }
+}
+
+ds = new Datasource('https://my-json-server.typicode.com/lihuicham/price-api/data');
+
 
